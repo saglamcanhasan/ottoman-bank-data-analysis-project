6b0278e7c18371c5b307b41b230ec175e312b1a7
saglamcanhasan <saglamcanhasan@gmail.com>
2025-08-06 21:51:50 +0300

    preprocessing

--- utils/historical_overview_analysis.py
+++ utils/historical_overview_analysis.py
@@ -6,19 +6,11 @@ def employee_count(start: int=1856, end: int=1922, end_inclusive: bool=False):
     print(employee_dataset.dtypes)
 
 def open_agencies(start: int=1856, end: int=1922, end_inclusive: bool=False):
-    # filter redundant values
-    df = agency_dataset.iloc[:, :3].copy()
-
-    # change rodos closing date
-    mask = df["City"] == "Rodos"
-    if mask.any():
-        closing_date = df.loc[mask, "Closing date"].values[0]
-        if isinstance(closing_date, str) and "," in closing_date:
-            second_date = closing_date.split(",")[1].strip()
-            df.loc[mask, "Closing date"] = second_date
+    # copy dataset
+    df = agency_dataset.copy()
 
     # change closing date type
-    df["Closing date"] = df["Closing date"].fillna(end+1).astype(int)
+    df["Closing date"] = df["Closing date"].fillna(str(end+1)).astype(int)
 
     # count open agencies
     open_agencies_df = pd.Series(0, dtype=int, index=np.arange(start, end+1))

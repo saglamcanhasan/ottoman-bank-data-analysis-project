132ff889eeb8199da4712252dcc9e4818a535ea7
saglamcanhasan <saglamcanhasan@gmail.com>
2025-08-14 12:34:02 +0300

    auth debug

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ import uvicorn
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from routers.router import router
+from fastapi.middleware.cors import CORSMiddleware
 
 # load port
 load_dotenv()
@@ -11,7 +12,16 @@ port = int(os.getenv("PORT"))
 # create app
 app = FastAPI()
 
-# middleware
+# middlewares
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://localhost:5001",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# routers
 app.include_router(router, prefix="/api")
 
 # start server
